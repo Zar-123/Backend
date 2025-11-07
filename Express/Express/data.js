95361b6f5770454528283de5a5221957037afe7e
Brian Q <brianqm88@gmail.com>
2025-11-07 13:23:58 -0300

    Add files via upload

---
+++ Express/Express/data.js
@@ -0,0 +1,10 @@
+
+export const users = [
+    { id: 'a1b2c3d4-e5f6-7890-1234-567890abcdef', name: 'Ana' },
+    { id: 'f0e9d8c7-b6a5-4321-fedc-ba9876543210', name: 'Luis' },
+    { id: '1a2b3c4d-5e6f-7890-abcd-ef0123456789', name: 'Carla' },
+];
+
+
+
+
---
+++ Express/Express/expressRouter.js
@@ -0,0 +1,38 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const userRouter = express.Router();
+
+userRouter.use((req, res, next) => {
+  console.log(`📦 Middleware del router → ${req.method} ${req.url}`);
+  next();
+});
+
+userRouter.get("/",(req,res) =>{
+    res.json([{id : 1 , name: 'User from router'}])
+});
+
+userRouter.get("/hola",(req,res) =>{
+    res.send("Hola desde el router")
+});
+
+userRouter.post("/", (req,res) =>{
+    const user = req.body;
+    user.id = Math.floor(Math.random() * 1000);
+    res.status(201).json(user);
+});
+
+app.use("/api/users", userRouter);
+
+app.get("/",(req,res) =>{
+    res.send('🏠 Bienvenido! Visitá /api/users para probar el router.');
+})
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`✅ Servidor Express con Router corriendo en http://localhost:${PORT}`);
+});
