e074e3138ac063664ee6397a727e0a5d85d401af
Brian Q <brianqm88@gmail.com>
2025-12-17 00:46:14 -0300

    Add files via upload

---
+++ ExpressV2/Express/servicios/servicioAuth.js
@@ -0,0 +1,41 @@
+import jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../express';
+
+const mockUserDb = [
+    {username: "admin", password: "contraseña123", id: "usuario-1", role: "admin"},
+    {username: "guest", password: "guest", id: "usuario-2", role: "guest"}
+]
+
+export function generarToken(username, password){
+    const user = mockUserDb.find(
+        u => u.username === username  && u.password === password
+    );
+
+    if (!user) {
+        const error = new Error("Credenciales inválidas. Usuario no encontrado o contraseña incorrecta.");
+        error.status = 401; // Unauthorized
+        throw error;
+    }
+    const payload = {
+        id: user.id,
+        username: user.username,
+        role: user.role
+    }
+
+    try {
+        const token = jwt.sign(payload, JWT_SECRET, {expiresIn: '1h'});
+
+        console.log(`✅ Token emitido para el usuario: ${user.username}`);
+
+        res.json({
+            message: "Login exitoso",
+            token: token,
+            expiresIn: "1 hora"
+        })
+    } catch (error) {
+        console.error("❌ Error al firmar el token JWT:", error);
+        res.status(500).json({error: "Error interno al generar el token"})
+        signError.status = 500;
+        throw signError;
+    }
+}
